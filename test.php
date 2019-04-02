b3ee1534c34153dd221e2056d8c5569d521652ba
Teri Eyenike <teyenike1@gmail.com>
2019-04-02 02:27:08 +0100

    show files using bootstrap

---
+++ test.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+    <meta charset="utf-8">
+    <head>
+        <title>Vivet Photo</title>
+        <meta name="description" content="A portfolio site for all my creative workflow and it exploit the photograph from different perspective using just my smartphone to carry out the task and edited everything using an app Snapseed">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="icon" href="images/PacerLogo.png">
+    </head>
+    <body>
+        <p>this is everything</p>
+        <?php
+            $age = 21;
+            $name = "Clark Benson";
+            $dateOfBirth = 1982;
+            $place = "Lagos";
+            echo "$name" . " was " . " born" . " in" . " $place";
+        ?>
+        
+    </body>
+</html>
